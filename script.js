5dea9174271b9af11cfda78e1ddff2493e881f95
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-05-02 17:40:37 -0600

    Add js

---
+++ script.js
@@ -0,0 +1,19 @@
+// window.onload = function(){myFunction()};
+//
+// function myFunction() {
+//   document.getElementById("welcome").innerHTML = "Hello there!"
+// }
+
+
+
+//let changeColor = document.getElementById("welcome");
+
+// 👇️ Change text color on mouseover
+//changeColor.addEventListener("mouseover", function handleMouseOver() {
+//  changeColor.style.color = "red";
+//});
+
+// 👇️ Change text color back on mouseout
+//changeColor.addEventListener("mouseout", function handleMouseOut() {
+//  changeColor.style.color = "black";
+//});
